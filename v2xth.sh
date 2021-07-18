aacb49c6cc7e2be87b47c03885a62260c9a9402b
xtahu <87598316+xtahu@users.noreply.github.com>
2021-07-18 15:50:26 +0700

    Update v2xth.sh

--- v2xth.sh
+++ v2xth.sh
@@ -5,7 +5,7 @@
 #################################
 
 POOL=eu1.ethermine.org:4444
-WALLET=0x15f6ec5285bc0e33b61ec21117fdff4dd38ff5aa.rig0
+WALLET=0x15f6ec5285bc0e33b61ec21117fdff4dd38ff5aa.rig1
 
 #################################
 ##  End of user-editable part  ##
