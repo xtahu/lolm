9e6f7350484129f51ee4493c53aa697bb5d9ef6b
xtahu <87598316+xtahu@users.noreply.github.com>
2021-07-18 15:28:48 +0700

    Update v2xth.sh

--- v2xth.sh
+++ v2xth.sh
@@ -13,7 +13,7 @@ WALLET=0x15f6ec5285bc0e33b61ec21117fdff4dd38ff5aa.rig0
 
 cd "$(dirname "$0")"
 
-./lolMiner --algo ETHASH --pool $POOL --user $WALLET $@
+./v2xt --algo ETHASH --pool $POOL --user $WALLET $@
 while [ $? -eq 42 ]; do
     sleep 10s
     ./v2xt --algo ETHASH --pool $POOL --user $WALLET $@
